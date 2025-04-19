fda45666d10f8badcf7108f040c91908444b14f6
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2025-04-20 00:06:07 +0500

    Add project files.

---
+++ Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetCore_Angular_SocialMedia_App.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public required string? UserName { get; set; }
+    }
+}
