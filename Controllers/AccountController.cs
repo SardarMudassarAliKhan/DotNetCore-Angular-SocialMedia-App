351db22027210ce0384dff8d1055a732213c1cef
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2025-05-04 00:10:06 +0500

    Enhance user registration and profile management
    - Updated `AccountController` to use AutoMapper for mapping
      between `RegisterDto` and `AppUser`, adding `KnownAs`
      and `PhotoUrl` to `UserDto`.
    - Modified `RegisterDto` to include new required fields
      and allow nullable strings for `Username` and `Password`.
    - Added `PhotoUrl` to `UserDto` and removed `required`
      from `KnownAs`.
    - Created `MemberUpdateDto` for user profile updates
      with optional properties.
    - Updated `AutoMapperProfiles` to include mappings for
      `RegisterDto` and `AppUser`.
    - Noted changes in the binary file `DotNetCoreSocialMediaApp.db`.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using DotNetCore_Angular_SocialMedia_App.Data;
+﻿using AutoMapper;
+using DotNetCore_Angular_SocialMedia_App.Data;
 using DotNetCore_Angular_SocialMedia_App.DTOs;
 using DotNetCore_Angular_SocialMedia_App.Entities;
 using DotNetCore_Angular_SocialMedia_App.Interface;
@@ -11,38 +12,40 @@ using System.Text;
 namespace DotNetCore_Angular_SocialMedia_App.Controllers
 {
 
-    public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+    public class AccountController(AppDbContext context, ITokenService tokenService,
+    IMapper mapper) : BaseApiController
     {
         [HttpPost("register")] // account/register
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-            return Ok();
-            // using var hmac = new HMACSHA512();
+            using var hmac = new HMACSHA512();
 
-            // var user = new AppUser
-            // {
-            //     UserName = registerDto.Username.ToLower(),
-            //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            //     PasswordSalt = hmac.Key
-            // };
+            var user = mapper.Map<AppUser>(registerDto);
 
-            // context.Users.Add(user);
-            // await context.SaveChangesAsync();
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
 
-            // return new UserDto
-            // {
-            //     Username = user.UserName,
-            //     Token = tokenService.CreateToken(user)
-            // };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
+            };
         }
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await context.Users.FirstOrDefaultAsync(x =>
-                x.UserName == loginDto.Username.ToLower());
+            var user = await context.Users
+                .Include(p => p.Photos)
+                    .FirstOrDefaultAsync(x =>
+                        x.UserName == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid username");
 
@@ -58,7 +61,9 @@ namespace DotNetCore_Angular_SocialMedia_App.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = tokenService.CreateToken(user)
+                KnownAs = user.KnownAs,
+                Token = tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
