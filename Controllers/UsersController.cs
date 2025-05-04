730758cf10f4d13e043c5964ae1d583fe6d9b5e3
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2025-05-05 00:49:35 +0500

    End of section 10
    Add AutoMapper support and update user functionality
    
    Updated `UsersController` to include AutoMapper for object mapping.
    Modified the constructor to accept an `IMapper` instance.
    Added `UpdateUser` method to handle HTTP PUT requests for user updates,
    including validation and response handling.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,12 +1,14 @@
-﻿using DotNetCore_Angular_SocialMedia_App.DTOs;
+﻿using AutoMapper;
+using DotNetCore_Angular_SocialMedia_App.DTOs;
 using DotNetCore_Angular_SocialMedia_App.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace DotNetCore_Angular_SocialMedia_App.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -24,6 +26,24 @@ namespace DotNetCore_Angular_SocialMedia_App.Controllers
             if (user == null) return NotFound();
 
             return user;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (username == null) return BadRequest("No username found in token");
+
+            var user = await userRepository.GetUserByUsernameAsync(username);
+
+            if (user == null) return BadRequest("Could not find user");
+
+            mapper.Map(memberUpdateDto, user);
+
+            if (await userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update the user");
         }
     }
 }
